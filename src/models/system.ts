7a742be35b051d394b107fa8169d3df24400c38f
anitayang-create <anita.yang@stockfeel.com.tw>
2026-07-20 11:18:28 +0800

    Delete src directory

--- src/models/system.ts
+++
@@ -1,8 +0,0 @@
-export interface AuditLog {
-  id: string;
-  actionTime: string;    // 操作時間
-  operator: string;      // 操作人員
-  module: string;        // 操作模組
-  action: string;        // 執行動作
-  target: string;        // 影響目標
-}
